0090754578688c34e26723df0a88b3e433519c44
nerdiejack <nerdiejack@gmail.com>
2018-12-03 23:39:55 +0100

    add more basic numpy array functions

--- numpy_array.py
+++ numpy_array.py
@@ -1,5 +1,8 @@
 # create array
 from numpy import array
+from numpy import empty
+from numpy import zeros
+from numpy import ones
 
 # create array
 l = [1.0, 2.0, 3.0]
@@ -10,4 +13,12 @@ print(a)
 print(a.shape)
 # display array data type
 print(a.dtype)
-
+# create empty array
+a = empty([3, 3])
+print(a)
+# create zero array
+a = zeros([3, 5])
+print(a)
+# create one array
+a = ones([5])
+print(a)
