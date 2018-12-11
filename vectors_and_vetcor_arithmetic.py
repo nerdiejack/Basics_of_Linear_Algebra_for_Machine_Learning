11ba600b8a53936b17754a86b86febf00f82ee2a
nerdiejack <nerdiejack@gmail.com>
2018-12-11 13:22:23 +0100

    vectors_and_vetcor_arithmetic

---
+++ vectors_and_vetcor_arithmetic.py
@@ -0,0 +1,45 @@
+# create a vector
+from numpy import array
+# define vector
+# a = array([1, 2, 3])
+# print(a)
+# b = array([1, 2, 3])
+# print(b)
+# c = a + b
+# print(c)
+
+# a = array([1, 2, 3])
+# print(a)
+# b = array([0.5, 0.5, 0.5])
+# print(b)
+# c = a - b
+# print(c)
+
+# a = array([1, 2, 3])
+# print(a)
+# b = array([1, 2, 3])
+# print(b)
+# c = a * b
+# print(c)
+
+# a = array([1, 2, 3])
+# print(a)
+# b = array([1, 2, 3])
+# print(b)
+# c = a / b
+# print(c)
+
+# vector dot product
+# a = array([1, 2, 3])
+# print(a)
+# b = array([1, 2, 3])
+# print(b)
+# c = a.dot(b)
+# print(c)
+
+a = array([1, 2, 3])
+print(a)
+s = 0.5
+print(s)
+c = s * a
+print(c)
