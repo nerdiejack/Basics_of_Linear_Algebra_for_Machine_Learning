b643c6aa41760aaab3a0536121df0bb00354caa1
nerdiejack <nerdiejack@gmail.com>
2018-12-11 17:42:19 +0100

    Matrix operations.

---
+++ matrix_operations.py
@@ -0,0 +1,38 @@
+# transpose matrix
+from numpy import array
+from numpy.linalg import inv
+from numpy.linalg import det
+from numpy import trace
+# define matrix
+# A = array([
+# [1, 2],
+# [3, 4],
+# [5, 6]])
+# print(A)
+# calculate transpose
+# C = A.T
+# print(C)
+
+# A = array([
+# [1.0, 2.0],
+# [3.0, 4.0]])
+# print(A)
+# invert matrix
+# B = inv(A)
+# print(B)
+# multiply A and B
+# I = A.dot(B)
+# print(I)
+
+# matrix trace
+# A = array([
+# [1, 2, 3],
+# [4, 5, 6],
+# [7, 8, 9]])
+# print(A)
+# calculate trace
+# B = trace(A)
+# print(B)
+
+
+
